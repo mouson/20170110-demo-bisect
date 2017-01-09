d53f159744357d82eb1e908171353211eb39d247
mouson <mouson@gmail.com>
2017-01-10 04:00:48 +0800

    Issue #1 增加測試：補充非零數測試

---
+++ tests/Mouson/Helpers/SequenceCalculator/DoubleSequenceCalculatorTest.php
@@ -0,0 +1,51 @@
+<?php
+namespace Mouson\Helpers\SequenceCalculator;
+use PHPUnit\Framework\TestCase;
+
+class DoubleSequenceCalculatorTest extends TestCase
+{
+    public function test_物件產生後應可以取得物件名稱()
+    {
+        /** Arrange */
+        $target = new DoubleSequenceCalculator();
+
+        /** Assume */
+        $expected = 'Double';
+
+        /** Act */
+        $actual = $target->getSequenceName();
+
+        /** Assert */
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_測試雙倍數列輸入0應回傳0()
+    {
+        /** Arrange */
+        $target = new DoubleSequenceCalculator();
+        $value  = 0;
+        /** Assume */
+        $expected = 0;
+
+        /** Act */
+        $actual = $target->calculate($value);
+
+        /** Assert */
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_測試雙倍數列輸入2應回傳4()
+    {
+        /** Arrange */
+        $target = new DoubleSequenceCalculator();
+        $value  = 2;
+        /** Assume */
+        $expected = 4;
+
+        /** Act */
+        $actual = $target->calculate($value);
+
+        /** Assert */
+        $this->assertEquals($expected, $actual);
+    }
+}
